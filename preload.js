83e42cfc8a68da31a695ff64ffa8fc8b5ba83c4d
Iooping <62954935+Iooping@users.noreply.github.com>
2025-09-04 16:34:47 +0800

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,7 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    startFileWatcher: () => ipcRenderer.send('start-file-watcher'),
-    onInitialFileList: (callback) => ipcRenderer.on('initial-file-list', (_event, files) => callback(files)),
-    onNewFileAdded: (callback) => ipcRenderer.on('new-file-added', (_event, filename) => callback(filename))
+  startFileWatcher: () => ipcRenderer.send('start-file-watcher'),
+  onInitialFileList: (callback) => ipcRenderer.on('initial-file-list', (_event, files) => callback(files)),
+  onNewFileAdded: (callback) => ipcRenderer.on('new-file-added', (_event, filename) => callback(filename))
 });
